39baf575a374daad16b8ce66894ffd01fcbbb219
oscar <oscar.vasquez@zentro.com.mx>
2024-03-12 15:19:24 -0600

    debug

--- app.js
+++ app.js
@@ -8,11 +8,33 @@ import imageminGifsicle from "imagemin-gifsicle";
 import sharp from "sharp";
 
 let inputFolder = "src";
-let ouputFolder = "opt";
+let outputFolder = "opt";
 let targetWidth = 1920;
 
 const processImg = async () => {
     try {
+
+        const files = await fse.readdir(inputFolder);
+
+        for(const file of files){
+            let inputPath = `${inputFolder}/${file}`; 
+            let outputPath = `${outputFolder}/${file}`;
+
+            await sharp(inputPath).resize(targetWidth).toFile(outputFolder);
+
+            await imagemin([outputPath],{
+                destination: outputFolder,
+                plugins: [
+                    imageminJpegtran({ quality: 80}), // Comprimir imagen JPG con calidad del 80%
+                    imageminPngquant(), // Comprimir imagen PNG 
+                    imageminSvgo(), // Comprimir imagen SVG 
+                    imageminWebp({ quality: 80}), // Comprimir imagen Webp con calidad del 80%
+                    imageminGifsicle(), // Comprimir imagen GIF 
+                ]
+            });
+
+            console.log("ha terminado con exito el proceso de optimizar tus imagenes")
+        }
 
     } catch (error) {
         console.log(error)
